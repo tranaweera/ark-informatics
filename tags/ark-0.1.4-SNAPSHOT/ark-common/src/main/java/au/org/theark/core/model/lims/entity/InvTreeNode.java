8fa738eeef34d51cf2ee34dafb537023af1e8e8d
elam <elam@2beaffb7-5388-1629-0768-26d37724f1fc>
2011-09-02 06:46:30 +0000

    Incorrectly performed 0.1.4-SNAPSHOT.

--- tags/ark-0.1.4-SNAPSHOT/ark-common/src/main/java/au/org/theark/core/model/lims/entity/InvTreeNode.java
+++
@@ -1,9 +0,0 @@
-package au.org.theark.core.model.lims.entity;
-
-import java.util.List;
-
-public interface InvTreeNode<T> {
-	public List<T> getChildren();
-	public String getNodeName();
-	public String getNodeType();
-}
--- tags/ark-0.1.4-SNAPSHOT/ark-database/db-scripts/patch/20110711_v0.1.3/mysql/20110721_study_ark_module_schemaPatch.sql
+++
@@ -1,5 +0,0 @@
-USE study;
-
--- Ensures the module names are unique
-ALTER TABLE `study`.`ark_module` 
-ADD UNIQUE INDEX `ARK_MODULE_NAME_UNIQUE` (`NAME` ASC) ;
