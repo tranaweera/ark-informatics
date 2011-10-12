96b13102f5fd403de7050f0a9ccbf00e429bec48
elam <elam@2beaffb7-5388-1629-0768-26d37724f1fc>
2011-10-12 02:25:26 +0000

    Removing because need to re-tag 0.1.5

--- tags/ark-0.1.5-SNAPSHOT/the-ark/ark-common/src/main/java/au/org/theark/core/web/component/panel/IDownloadRequestHandlerProvider.java
+++
@@ -1,9 +0,0 @@
-package au.org.theark.core.web.component.panel;
-
-import org.apache.wicket.request.IRequestHandler;
-
-public interface IDownloadRequestHandlerProvider {
-	
-	public abstract IRequestHandler getDownloadRequestHandler();
-
-}
